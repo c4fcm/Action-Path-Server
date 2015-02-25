2fd5ef213bd2d627d145f270edfec5b045795779
Edward L Platt <ed@elplatt.com>
2015-02-25 16:42:08 -0500

    Add routes for resources.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  resources :users, :issues, :responses, :subscriptions
   
-  post 'users' => 'users#create'
-  get 'users/:id' => 'users#show'
-
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
