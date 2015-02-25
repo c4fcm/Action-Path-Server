75c061017d4e4de781689f9c2bf7d0a8407b07ca
Edward L Platt <ed@elplatt.com>
2015-02-25 17:16:31 -0500

    Add IssuesController#show with fake data.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,2 +1,15 @@
 class IssuesController < ApplicationController
+  def show
+    #@issue = Issue.find(params[:id])
+    @issue = {
+      :status => "Open",
+      :summary => "Pothole",
+      :description => "Please fix my neighborhood.",
+      :lat => 42.30293,
+      :lng => -72.234234234,
+      :address => "123 State St. New Haven, CT",
+      :image_full => "http://seeclickfix.com/files/comment_images/0001/3476/32eebb4f8669b5beb441280bc16f26bf.jpeg"
+    }
+    render :json => @issue
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150218234243) do
+ActiveRecord::Schema.define(version: 20150225214646) do
+
+  create_table "issues", force: :cascade do |t|
+    t.string   "status"
+    t.string   "summary"
+    t.string   "description"
+    t.decimal  "lat"
+    t.decimal  "lng"
+    t.text     "address"
+    t.string   "image_full"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at", null: false
