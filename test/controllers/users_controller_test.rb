1ba4152f85fb11a610f54bc3877ebffe0adadcd7
Edward L Platt <ed@elplatt.com>
2015-02-25 16:25:13 -0500

    Change users post test to look for redirect.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -3,7 +3,7 @@ require 'test_helper'
 class UsersControllerTest < ActionController::TestCase
   test "should get create" do
     get :create
-    assert_response :success
+    assert_redirected_to user_path(assigns(:user))
   end
 
 end
