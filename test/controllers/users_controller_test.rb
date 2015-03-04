7a9b9e489ea0a678e329b29224649dc9bd309e94
Edward L Platt <ed@elplatt.com>
2015-03-04 16:48:02 -0500

    Complete tests for user requests.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,9 +1,14 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionController::TestCase
-  test "should get create" do
-    get :create
+  test "should create user" do
+    assert_difference('User.count') do
+      post :create
+    end
     assert_redirected_to user_path(assigns(:user))
   end
-
+  test "sould get user" do
+    get :show, {:id => users(:one).id}
+    assert_response :success
+  end
 end
