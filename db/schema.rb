c77f5db488e068e9c5cc4a71827ba6cf5813f478
Edward L Platt <ed@elplatt.com>
2015-03-04 12:03:05 -0500

    Add skeleton for subscription and associations with user/issue.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150225214646) do
+ActiveRecord::Schema.define(version: 20150304165359) do
 
   create_table "issues", force: :cascade do |t|
     t.string   "status"
@@ -23,6 +23,11 @@ ActiveRecord::Schema.define(version: 20150225214646) do
     t.string   "image_full"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "subscriptions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "users", force: :cascade do |t|
