3140b08b0c1fe7f83fad7d8dda1ec922b560b40e
Edward L Platt <ed@elplatt.com>
2015-03-11 16:07:35 -0400

    Fix subscription destroy action.

--- test/controllers/subscriptions_controller_test.rb
+++ test/controllers/subscriptions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class SubscriptionsControllerTest < ActionController::TestCase
   test "should delete" do
-    assert_difference('Subscription.count') do
+    assert_difference('Subscription.count', -1) do
       delete :destroy, {:id => subscriptions(:one_pothole).id}
     end
     assert_response :no_content
