7e3e25db9c99c6697ddc017f2af86bde8b61259e
Edward L Platt <ed@elplatt.com>
2015-03-11 13:59:26 -0400

    Fix typo in user controller test.

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -7,7 +7,7 @@ class UsersControllerTest < ActionController::TestCase
     end
     assert_redirected_to user_path(assigns(:user))
   end
-  test "sould get user" do
+  test "should get user" do
     get :show, {:id => users(:one).id}
     assert_response :success
   end
