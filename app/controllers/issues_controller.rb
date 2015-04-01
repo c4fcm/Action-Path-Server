eb4fd64850a3b3386127f0f1ba4c9481a994a42a
Edward L Platt <ed@elplatt.com>
2015-04-01 14:36:30 -0400

    Replace dummy data with actual for issue endpoint.

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -22,16 +22,7 @@ class IssuesController < ApplicationController
     render :json => @issues
   end
   def show
-    #@issue = Issue.find(params[:id])
-    @issue = {
-      :status => "Open",
-      :summary => "Pothole",
-      :description => "Please fix my neighborhood.",
-      :lat => 42.30293,
-      :lng => -72.234234234,
-      :address => "123 State St. New Haven, CT",
-      :image_full => "https://pbs.twimg.com/media/BrTlTtRIEAAU7jv.jpg:large"
-    }
+    @issue = Issue.find(params[:id])
     render :json => @issue
   end
 end
