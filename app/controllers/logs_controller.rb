fb256cad6ad5e638f58f98f09ea823e3f037d40b
Rahul Bhargava <rahulb@media.mit.edu>
2015-06-16 11:26:58 -0400

    try to save log msgs to DB

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -4,6 +4,10 @@ class LogsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def create
     data = JSON.parse params[:data]
+    data.each do |json_log|
+    	log = Log.from_json_obj json_log
+    	log.save
+    end
 	render :json => data
   end
 end
--- app/models/log.rb
+++ app/models/log.rb
@@ -1,2 +1,15 @@
 class Log < ActiveRecord::Base
+
+	def from_json_obj obj
+		log = Log.new
+		log.type = "action"
+		log.user_id = Integer(obj.userID)
+		log.issue_id = Integer(obj.issueID)
+		log.action = obj.actionType
+		log.timestamp = DateTime.parse(obj.timestamp)
+		log.lat = Float(log.lat)
+		log.lng = Float(log.long)
+		log
+	end
+
 end
