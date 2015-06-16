339f1ed8e3600c6ce44553bf1f2d65623e69f689
Rahul Bhargava <rahulb@media.mit.edu>
2015-06-16 11:31:20 -0400

    saving log db msgs

--- app/models/log.rb
+++ app/models/log.rb
@@ -3,12 +3,12 @@ class Log < ActiveRecord::Base
 	def self.from_json_obj obj
 		log = Log.new
 		log.type = "action"
-		log.user_id = Integer(obj.userID)
-		log.issue_id = Integer(obj.issueID)
-		log.action = obj.actionType
-		log.timestamp = DateTime.parse(obj.timestamp)
-		log.lat = Float(log.lat)
-		log.lng = Float(log.long)
+		log.user_id = Integer(obj["userID"])
+		log.issue_id = Integer(obj.["issueID"])
+		log.action = obj["actionType"]
+		log.timestamp = DateTime.parse(obj.["timestamp"])
+		log.lat = Float(log["lat"])
+		log.lng = Float(log["long"])
 		log
 	end
 
