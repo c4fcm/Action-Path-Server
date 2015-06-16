0238164143772be11b7bfbe5efda45fd6b7b63c4
Rahul Bhargava <rahulb@media.mit.edu>
2015-06-16 10:14:00 -0400

    echo back log create test

--- app/controllers/logs_controller.rb
+++ app/controllers/logs_controller.rb
@@ -3,6 +3,7 @@ require 'json'
 class LogsController < ApplicationController
   skip_before_filter :verify_authenticity_token
   def create
-    render :json => params 
+    data = JSON.parse params[:data]
+	render :json => data
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
     resources :subscriptions
   end
   
-  post 'logs' => 'logs#create'
+  post 'logs/create' => 'logs#create'
   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
