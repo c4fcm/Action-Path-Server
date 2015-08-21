04533b0e79edb7c6a6442650f77ad630e25d82a3
Rahul Bhargava <rahulb@media.mit.edu>
2015-08-21 10:10:03 -0400

    use the right model!

--- app/models/response.rb
+++ app/models/response.rb
@@ -2,7 +2,7 @@ class Response < ActiveRecord::Base
 
 	def self.from_json_obj obj
 		install = Install.get_or_create(obj["installId"]).id
-		response = Log.new
+		response = Response.new
 		response.answer = obj["answerText"]
 		response.install_id = install.id
 		response.issue_id = Integer(obj["issueId"])
