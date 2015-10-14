0c9552b437a5ecfead8447b136fb378696d01d38
Rahul Bhargava <rahulb@media.mit.edu>
2015-10-14 16:21:30 -0400

    save response comment

--- app/models/response.rb
+++ app/models/response.rb
@@ -7,6 +7,7 @@ class Response < ActiveRecord::Base
 		install = Install.get_or_create(obj["installId"])
 		response = Response.new
 		response.answer = obj["answerText"]
+		response.comment = obj["comment"]
 		response.install_id = install.id
 		response.issue_id = Integer(obj["issueId"])
 		response.timestamp = Time.at(obj["timestamp"].to_i).to_datetime
