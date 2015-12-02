1be81ad011557300dee9aaf6a1b6ead1dd35a66f
Rahul Bhargava <rahulb@media.mit.edu>
2015-12-02 09:55:04 -0500

    adding is_real column

--- db/schema.rb
+++ db/schema.rb
@@ -11,14 +11,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151118194003) do
+ActiveRecord::Schema.define(version: 20151118210838) do
 
   create_table "installs", force: :cascade do |t|
     t.integer  "user_id",      limit: 4
-    t.datetime "created_at",                 null: false
-    t.datetime "updated_at",                 null: false
+    t.datetime "created_at",                                 null: false
+    t.datetime "updated_at",                                 null: false
     t.text     "device_id",    limit: 65535
     t.string   "request_type", limit: 255
+    t.boolean  "is_real",                    default: false
   end
 
   create_table "issues", force: :cascade do |t|
